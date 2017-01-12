3260ba1fb2e44d67cbef5fabbc536dd699bf8879
Facundo Flores <fflores@jbknowledge.com>
2017-01-12 15:09:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../node_modules/spectre.css/dist/spectre.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pedidos APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
