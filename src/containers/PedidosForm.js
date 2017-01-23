1bda412a8ef268f3804fc40d51fcfda72a1ddb87
Facundo Flores <fflores@jbknowledge.com>
2017-01-23 15:14:25 -0300

    Add custom validation

--- src/containers/PedidosForm.js
+++ src/containers/PedidosForm.js
@@ -2,12 +2,27 @@ import React, { PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import SelectInput from '../components/SelectInput';
 
-const renderNumberField = field => (
-  <div>
-    <label className="form-label" htmlFor="input-cantidad">Cantidad</label>
-    <input {...field.input} className="form-input" type="number" id="input-cantidad" placeholder="Cantidad" />
-  </div>
-);
+const validate = (values) => {
+  const errors = {};
+  if (!values.myNumberField) {
+    errors.myNumberField = 'Requerido';
+  } else if (isNaN(Number(values.myNumberField))) {
+    errors.myNumberField = 'Debe ingresar un número';
+  } else if (Number(values.myNumberField) > 3) {
+    errors.myNumberField = 'No puede pedir más de 3 comidas';
+  } else if (Number(values.myNumberField) < 0) {
+    errors.myNumberField = 'Debe ingresar un número positivo';
+  }
+  return errors;
+};
+
+const warn = (values) => {
+  const warnings = {};
+  if (values.myNumberField > 2) {
+    warnings.myNumberField = 'Llegando al límite...';
+  }
+  return warnings;
+};
 
 const renderSelect = field => (
   <SelectInput
@@ -17,13 +32,23 @@ const renderSelect = field => (
   />
 );
 
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} className="form-input" type="number" id="input-cantidad" placeholder={label} />
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+);
+
 class PedidosForm extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, pristine, reset, submitting } = this.props;
     const options = [
         { label: 'Hamburguesa', value: 'HAM' },
         { label: 'Pizza', value: 'PIZ' },
@@ -39,7 +64,7 @@ class PedidosForm extends React.Component {
             component={renderSelect}
             options={options}
           />
-          <Field name="myNumberField" component={renderNumberField} />
+          <Field name="myNumberField" type="number" component={renderField} label="Ingrese cantidad" />
         </div>
         <div className="form-group">
           <button type="submit" className="btn btn-primary input-group-btn">Submit</button>
@@ -55,6 +80,8 @@ PedidosForm.propTypes = {
 
 PedidosForm = reduxForm({
   form: 'pedidos',
+  validate,
+  warn,
 })(PedidosForm);
 
 export default PedidosForm;
